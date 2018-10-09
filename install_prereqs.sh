d72faa73fe999d54f720ecc3112b4a062cf91ef6
caioycosta <caioycosta@github>
2018-10-09 09:32:58 -0300

    versao        go

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install build-essential autoconf libtool make openjdk-8-jdk python-setuptools pkg-config golang
+sudo apt install ant build-essential autoconf libtool make openjdk-8-jdk python-setuptools pkg-config golang
 
 
 

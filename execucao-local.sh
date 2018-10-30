62c298a3fe5e84c63f46c8537a7e59663565c66d
caioycosta <caioycosta@github>
2018-10-30 15:33:14 -0300

    teste

--- execucao-local.sh
+++ execucao-local.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-ssh -o "StrictHostKeyChecking no" localhost /bin/bash << EOF | sed 's/^/SSH1:/' &
+set -b
+
+ssh -o "StrictHostKeyChecking no" localhost /bin/bash << EOF 2>&1 | sed 's/^/SSH1:/' &
 echo Teste
-echo Teste2 
+>&2 echo Teste2ERR 
 echo Teste3
+sleep 10
+echo quitting
 EOF
+JOB1=$! 
+
 #top -b -d 7 &
 echo Sleeping.
-sleep 30
+sleep 7 
 #kill %1
+kill $JOB1
 

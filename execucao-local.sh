bbab7bd251da9f2af62dade42a590d659f838833
caioycosta <caioycosta@github>
2018-10-30 09:41:10 -0300

    scripts

---
+++ execucao-local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ssh -o "StrictHostKeyChecking no" localhost /bin/bash & << EOF | sed 's/^/SSH1:/'
+echo Teste
+echo Teste2 
+echo Teste3
+EOF
+#top -b -d 7 &
+echo Sleeping.
+sleep 30
+#kill %1
+

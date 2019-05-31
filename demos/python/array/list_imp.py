1ee0a424dee79c3cc73b3d1796c55098f80c8705
caioycosta <caioycosta@github>
2019-05-31 14:12:46 -0300

    ajustes

--- demos/python/array/list_imp.py
+++ demos/python/array/list_imp.py
@@ -5,19 +5,31 @@ import logging
 import Estado_pb2
 import Request_pb2
 import Response_pb2
+import monotonic
 from bftsmart_serverlib import *
 
 class BFTList(BFTSMaRtServer):
-    def __init__(self,classpth,id,dllpath,respsize):
+    def __init__(self,classpth,id,dllpath,interval,respsize):
         super(BFTList,self).__init__(classpth,id,dllpath)
         self.respsize = respsize
+        self.rqst_count = 0
+        self.interval = interval
+        self.start_time = 0
 
     def execute(self, input):
+        if self.start_time == 0:
+            self.start_time = monotonic.monotonic()
         logging.info("execute")
         req = Request_pb2.Request()
         req.ParseFromString(input)
         rsp = Response_pb2.Response()
 	rsp.Data = bytearray(self.respsize)
+        self.rqst_count++;
+        if self.rqst_count >= self.interval:
+            now = monotonic.monotonic()
+            print "Throughput: {0} /s".format(self.rqst_count / now - self.start_time)
+            self.rqst_count = 0
+            self.start_time = monotonic.monotonic()
         return rsp.SerializeToString()
 
     def invokeOrdered(self, input):

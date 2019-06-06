d8e31b8fc5febcd355fd5d0d759aefbbe481f1dc
Caio <caioycosta@github>
2019-06-06 14:09:23 -0300

    emulab2

---
+++ execucao-array-emulab.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -b # reporta status de jobs imediatamente ao inves de so no fim do script
+
+ssh -t -t -o "StrictHostKeyChecking no" caioysc@node0.expteste.freestore.emulab.net /bin/bash << EOF 2>&1 | sed 's/^/SSH0:/' &
+cd bftsmart-diversity
+sleep 30
+./demos/java/run_array_client.sh 0 $1 $2 $3 
+EOF
+JOB1=$! 
+
+sleep 1
+
+ssh -t -t -o "StrictHostKeyChecking no" caioysc@node1.expteste.freestore.emulab.net /bin/bash << EOF 2>&1 | sed 's/^/SSH1:/' &
+cd bftsmart-diversity
+./demos/$5/run_array_server.sh 0 $3 $4
+EOF
+JOB2=$! 
+
+sleep 1
+
+ssh -t -t -o "StrictHostKeyChecking no" caioysc@node2.expteste.freestore.emulab.net /bin/bash << EOF 2>&1 | sed 's/^/SSH2:/' &
+cd bftsmart-diversity
+./demos/$5/run_array_server.sh 0 $3 $4
+EOF
+JOB3=$! 
+
+sleep 1
+
+ssh -t -t -o "StrictHostKeyChecking no" caioysc@node3.expteste.freestore.emulab.net /bin/bash << EOF 2>&1 | sed 's/^/SSH3:/' &
+cd bftsmart-diversity
+./demos/$5/run_array_server.sh 0 $3 $4
+EOF
+JOB4=$! 
+
+sleep 1
+
+ssh -t -t -o "StrictHostKeyChecking no" caioysc@node4.expteste.freestore.emulab.net /bin/bash << EOF 2>&1 | sed 's/^/SSH4:/' &
+cd bftsmart-diversity
+./demos/$5/run_array_server.sh 0 $3 $4
+EOF
+JOB5=$! 
+
+#top -b -d 7 &
+echo Sleeping.
+sleep 120 
+#kill %1
+kill $JOB1
+kill $JOB2
+kill $JOB3
+kill $JOB4
+kill $JOB5
+echo "Finalizado."
+
---
+++ setup-array-emulab.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -b # reporta status de jobs imediatamente ao inves de so no fim do script
+
+ssh -t -t -o "StrictHostKeyChecking no" caioysc@node0.expteste.freestore.emulab.net /bin/bash << EOF 2>&1 | sed 's/^/SSH0:/' 
+rm -rf bftsmart-diversity
+git clone https://github.com/caioycosta/bftsmart-diversity.git --recursive
+cd bftsmart-diversity
+cd config
+mv hostsEmulab.config hosts.config
+cd ..
+make -j10
+echo $? 
+EOF
+
+echo "Finalizado."
+

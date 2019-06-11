f9669d82e81f4e47e0508a62a64dc6f87310471f
Caio <caioycosta@github>
2019-06-11 09:56:01 -0300

    ajustes

--- execucao-completa.sh
+++ execucao-completa.sh
@@ -5,10 +5,6 @@ for i in python java c cpp go
 do
 for j in 0 512 1024 4096
 do
-echo
-date
-echo Caso $i $j
-./execucao-array-emulab.sh 100 2000 $j 5000 $i | grep -E '(LATENCY|STNDDEV|MAXTHRO)'
 for k in 0 1 2 3 4
 do
 ssh -t -t -o "StrictHostKeyChecking no" caioysc@node$k.expteste.freestore.emulab.net /bin/bash << EOF 2>&1 
@@ -16,6 +12,10 @@ killall -KILL /usr/bin/python
 exit
 EOF
 done
+echo
+date
+echo Caso $i $j
+./execucao-array-emulab.sh 100 2000 $j 5000 $i | grep -E '(LATENCY|STNDDEV|MAXTHRO)'
 echo
 done
 done
